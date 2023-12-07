3e534d67493433cc0af791ba59d79a16d2f86f2b
Noel Ohaeri <noelohaeri@gmail.com>
2023-12-07 00:21:33 -0500

    Environment variable change

--- app/lib/pusher.ts
+++ app/lib/pusher.ts
@@ -3,7 +3,7 @@ import PusherClient from 'pusher-js';
 
 export const pusherServer = new PusherServer({
     appId: process.env.Pusher_app_id!,
-    key: process.env.Pusher_key!,
+    key: process.env.NEXT_PUBLIC_Pusher_key!,
     secret: process.env.Pusher_secret!,
     cluster: process.env.Pusher_cluster!,
     useTLS: true,
