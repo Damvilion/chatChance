8b942a0006f75446d97b8572b54e27e8ca546e8a
Noel Ohaeri <noelohaeri@gmail.com>
2023-12-06 19:27:29 -0500

    Api routes

--- app/api/startmatch/route.ts
+++ app/api/startmatch/route.ts
@@ -1,1 +1,5 @@
-export async function POST(request: Request) {}
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+    return NextResponse.json({ message: 'Success' });
+}
