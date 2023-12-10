a66a30410c8a831bfd2c74ef34eecd8e86ed58b5
Noel Ohaeri <noelohaeri@gmail.com>
2023-12-10 16:56:22 -0500

    Ref: changed total user count technique

--- app/matchmaking/page.tsx
+++ app/matchmaking/page.tsx
@@ -1,44 +1,33 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { pusherClient } from '@/app/lib/pusher';
-import axios from 'axios';
-
-import { getPlayer } from './getPlayers';
+import { redisClient } from '@/app/lib/redis';
 
 const Page = () => {
-    // const helper = async () => {
-    //     const playerNumber = await getPlayer();
-    //     console.log(playerNumber);
-    // };
-    // helper();
     const [players, setPlayers] = useState<number | string>(0);
-    const getTotalPlayers = async () => {
-        const res = await axios.post('/api/getTotalPlayers', {
-            headers: {
-                'Cache-Control': 'no-cache',
-                Pragma: 'no-cache',
-            },
-        });
-
-        console.log(res);
-
-        if (!res) {
-            setPlayers('Error finding number of user');
-        } else {
-            setPlayers(res.data.channelsInfo.online_users.subscription_count);
-        }
-    };
-    pusherClient.subscribe('online_users');
 
     useEffect(() => {
-        getTotalPlayers();
+        const channel = pusherClient.subscribe('online_users');
+        channel.bind('pusher:subscription_count', (data: { subscription_count: React.SetStateAction<string | number> }) => {
+            setPlayers(data.subscription_count);
+        });
+        pusherClient.connection.bind('disconnected', () => {
+            console.log('you have been disconnected');
+        });
     }, []);
 
+    // const HandleDisconnect = () => {
+    //     pusherClient.disconnect();
+    // };
+
+    const logPusherUser = () => {
+        console.log(pusherClient.connection.socket_id);
+    };
     return (
         <div>
             <h1>Page</h1>
-            <p>There are {players} Online!</p>
-            {/* <button onClick={getTotalPlayers}>Get Players</button> */}
+            <button onClick={logPusherUser}>Disconnect</button>
+            {players}
         </div>
     );
 };
