88d50d1ccc132e89b856258798ebb148b13e19be
Noel Ohaeri <noelohaeri@gmail.com>
2023-12-13 04:41:15 -0500

    Proper redis setup

--- app/lib/redis.ts
+++ app/lib/redis.ts
@@ -1,10 +1,18 @@
 import { createClient } from 'redis';
 
+// const client = createClient({
+//     password: process.env.REDIS_PASSWORD,
+//     socket: {
+//         host: process.env.REDIS_HOST,
+//         port: process.env.REDIS_PORT!,
+//     },
+// });
+
 const client = createClient({
-    password: process.env.REDIS_PASSWORD,
+    password: process.env.NEXT_PUBLIC_REDIS_PASSWORD as string,
     socket: {
-        host: process.env.REDIS_HOST,
-        port: parseInt(process.env.REDIS_PORT!),
+        host: process.env.NEXT_PUBLIC_REDIS_HOST as string,
+        port: process.env.NEXT_PUBLIC_REDIS_PORT as unknown as number,
     },
 });
 
