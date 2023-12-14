7afb9eb4a3ba6bcea34f2b5f5b825285172a0637
Noel Ohaeri <noelohaeri@gmail.com>
2023-12-14 05:46:09 -0500

    updated comment

--- app/api/startmatch/route.ts
+++ app/api/startmatch/route.ts
@@ -3,7 +3,7 @@ import { redisClient } from '@/app/lib/redis';
 import { pusherServer } from '@/app/lib/pusher';
 
 export async function POST(request: Request) {
-    // Delete all online users
+    // Delete all previous online users
     await redisClient.del('online_users');
     const channels = ['matchmaking'];
     // Validates all users with pusher
