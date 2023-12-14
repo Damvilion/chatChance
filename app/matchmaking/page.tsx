5281743597ef216f8aa178b5367a97f0f4ddc84d
Noel Ohaeri <noelohaeri@gmail.com>
2023-12-14 05:48:43 -0500

    build(matchmaking): Added matchmaking state. Allows users to exist and enter matchmaking

--- app/matchmaking/page.tsx
+++ app/matchmaking/page.tsx
@@ -1,29 +1,52 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { pusherClient } from '@/app/lib/pusher';
 import axios from 'axios';
 
 const Page = () => {
     const [players, setPlayers] = useState<number | string>(0);
-    const [loading, setLoading] = useState<boolean>(true);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [matchmaking, setMatchmaking] = useState<boolean>(false);
+    // intervalID is used to clear the interval when the component unmounts
+    let intervalID = useRef<NodeJS.Timeout | null>(null);
 
+    const logChannels = () => {
+        console.log(pusherClient.channels.channels);
+    };
+
+    // This function is used to stop the matchmaking process
+    const stopMatching = () => {
+        setMatchmaking(false);
+        setLoading(false);
+        clearInterval(intervalID.current!);
+        pusherClient.unsubscribe('matchmaking');
+    };
+
+    // This useEffect hook is used to subscribe to the online_users channel
     useEffect(() => {
         // ALL Users are subscribed to this channel
         const channel = pusherClient.subscribe('online_users');
-        // Keeps track of the number of users subscribed to the channel
+        // Keeps track of the number of users subscribed to the online_users channel
         channel.bind('pusher:subscription_count', (data: { subscription_count: React.SetStateAction<string | number> }) => {
+            // update the number of players
             setPlayers(data.subscription_count);
         });
-
+        // When a user disconnects from the online_users channel
         pusherClient.connection.bind('disconnected', () => {
             console.log('you have been disconnected');
         });
     }, []);
 
-    const startmatch = async () => {
-        setLoading(true);
-        // Subscribe to the matchmaking channel
+    // Subscribe to the matchmaking channel | When matchmaking is true | handles matchmaking logic
+    useEffect(() => {
+        if (!matchmaking && intervalID.current) {
+            clearInterval(intervalID.current);
+            return;
+        }
+
         const matching = pusherClient.subscribe('matchmaking');
+
+        console.log('subscribed to matchmaking channel');
 
         // Fires a post request when pusherjs validates all users | This updates the redis database
         matching.bind('Validating all Users', async (data: string) => {
@@ -32,11 +55,18 @@ const Page = () => {
             });
         });
 
-        // API endpoint to validate all users
-        const res = await axios.post('/api/startmatch', {});
-        // Matchmaking has started | This will continue to run until a match is found
+        matching.bind('match_found', (data: { user1: string; user2: string }) => {
+            // When a match is found | This will only run for the two users that are matched
+            if (data.user1 === pusherClient.connection.socket_id || data.user2 === pusherClient.connection.socket_id) {
+                pusherClient.unsubscribe('matchmaking');
+                stopMatching();
+                console.log("user1's socket_id: " + data.user1);
+                console.log("user2's socket_id: " + data.user2);
+            }
+        });
+
         const findMatch = async () => {
-            while (true) {
+            if (matchmaking) {
                 const Users = await axios.post('/api/getplayers');
                 // This returns an array of all users
                 const ALL_USERS = Users.data.allUsers;
@@ -48,18 +78,38 @@ const Page = () => {
                     const randomUser = ALL_USERS[Math.floor(Math.random() * ALL_USERS.length)];
                     if (randomUser !== pusherClient.connection.socket_id) {
                         // MATCH FOUND
-                        console.log('Your Socket ID: ' + pusherClient.connection.socket_id);
-                        console.log('Random User: ' + randomUser);
+                        // Emit an event to the matchmaking channel
+                        matching.emit('match_found', {
+                            user1: pusherClient.connection.socket_id,
+                            user2: randomUser,
+                        });
+                        setMatchmaking(false);
                         setLoading(false);
                         pusherClient.unsubscribe('matchmaking'); // Unsubscribe from the matchmaking channel
-                        break; // Exit the loop when a match is found
+                        // Exit the loop when a match is found
                     }
                 }
-
-                await new Promise((resolve) => setTimeout(resolve, 1000)); // Wait for 1 second before checking again
             }
         };
-        findMatch();
+        // Keeps checking for a match every 2 seconds
+        intervalID.current = setInterval(findMatch, 1000);
+
+        return () => {
+            if (intervalID.current) {
+                clearInterval(intervalID.current);
+            }
+            pusherClient.unsubscribe('matchmaking');
+        };
+    }, [matchmaking]);
+
+    // This function is used to start the matchmaking process
+    const startmatch = async () => {
+        setLoading(true);
+        setMatchmaking(true);
+
+        // API endpoint to validate all users
+        await axios.post('/api/startmatch', {});
+        // Matchmaking has started | This will continue to run until a match is found
     };
 
     const logSocketID = () => {
@@ -71,11 +121,17 @@ const Page = () => {
             <h1>Page</h1>
             <div className='flex flex-col gap-4 justify-center items-center'>
                 {players}
-                <button className='bg-blue-400 p-3 rounded-lg' onClick={startmatch}>
+                <button className={`${loading ? 'bg-slate-500' : 'bg-blue-400'} p-3 rounded-lg`} onClick={startmatch}>
                     Start Match
                 </button>
                 <button className='bg-red-500 p-3 rounded-lg' onClick={logSocketID}>
                     Log Socket_id
+                </button>
+                <button className='bg-red-500 p-3 rounded-lg' onClick={stopMatching}>
+                    stop matching
+                </button>
+                <button className='bg-teal-500 p-3 rounded-lg' onClick={logChannels}>
+                    log Channels
                 </button>
             </div>
         </div>
