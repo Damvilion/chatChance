ebc6dab860b0600b9cfc484cbf5683aee1385e7e
Noel Ohaeri <noelohaeri@gmail.com>
2023-12-16 14:09:29 -0500

    Removed unncessary import

--- app/matchmaking/layout.tsx
+++ app/matchmaking/layout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { pusherServer } from '@/app/lib/pusher';
 
 export default function MatchMakingLayout({ children }: { children: React.ReactNode }) {
-    return <div>{children}</div>;
+    return <section>{children}</section>;
 }
