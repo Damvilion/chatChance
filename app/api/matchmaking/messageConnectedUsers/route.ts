4b7ee2517abb3d3e061555f6ddb07b308175a7b4
Noel Ohaeri <noelohaeri@gmail.com>
2023-12-16 14:05:52 -0500

    Ref: changed pusherchannel, added types for data

--- app/api/matchmaking/messageConnectedUsers/route.ts
+++ app/api/matchmaking/messageConnectedUsers/route.ts
@@ -1,22 +1,23 @@
 import { pusherServer } from '@/app/lib/pusher';
 import { NextResponse } from 'next/server';
 
+type data = {
+    socket_id: string;
+    randomUser: string;
+};
 // API Route to get the number of online users
 export async function POST(request: Request) {
-    const data = await request.json();
+    const data: data = await request.json();
     const { socket_id: user1, randomUser: user2 } = data;
 
-    const channels = ['online_users'];
+    const channels = ['matchmaking'];
 
     const res = await pusherServer.trigger(channels, 'match_found', { user1, user2 });
 
     if (res.status === 200) {
-        const body = await res.json();
-        const channelsInfo = body.channels;
-
         return NextResponse.json({
-            body,
-            channelsInfo,
+            message: 'Success',
+            status: 200,
         });
     } else {
         return NextResponse.json({ message: 'Failed', status: 400 });
