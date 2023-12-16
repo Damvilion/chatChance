cd637072133cc7b7564aa1a3ef8556708e1555e8
Noel Ohaeri <noelohaeri@gmail.com>
2023-12-16 14:09:00 -0500

    Added comments to further explain code. Some error handling changes

--- app/api/matchmaking/updateRedis/route.ts
+++ app/api/matchmaking/updateRedis/route.ts
@@ -2,13 +2,14 @@ import { NextResponse } from 'next/server';
 import { redisClient } from '@/app/lib/redis';
 
 export async function POST(request: Request) {
+    // Get the socket ID from the request body
     const body = await request.json();
-
+    // Add the socket ID to the online_users set | this is to add the user to the matchmaking pool
     try {
         await redisClient.sAdd('online_users', body.socket_id);
         return NextResponse.json({ message: 'Success' });
     } catch (error) {
         console.error('Error adding socket ID to online_users set:', error);
-        return NextResponse.json({ message: 'Failed' });
+        return NextResponse.json({ message: 'Failed', error, status: 400 });
     }
 }
