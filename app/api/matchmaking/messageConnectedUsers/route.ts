7318be437d3a3c5a83682abcb4ad412386c615a0
Noel Ohaeri <noelohaeri@gmail.com>
2023-12-17 17:05:57 -0500

    Updated POST data

--- app/api/matchmaking/messageConnectedUsers/route.ts
+++ app/api/matchmaking/messageConnectedUsers/route.ts
@@ -4,15 +4,16 @@ import { NextResponse } from 'next/server';
 type data = {
     socket_id: string;
     randomUser: string;
+    room: string;
 };
 // API Route to get the number of online users
 export async function POST(request: Request) {
     const data: data = await request.json();
-    const { socket_id: user1, randomUser: user2 } = data;
+    const { socket_id: user1, randomUser: user2, room } = data;
 
     const channels = ['matchmaking'];
 
-    const res = await pusherServer.trigger(channels, 'match_found', { user1, user2 });
+    const res = await pusherServer.trigger(channels, 'match_found', { user1, user2, room });
 
     if (res.status === 200) {
         return NextResponse.json({
