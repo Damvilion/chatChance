416dd18366d9c65f762e47d53ed19039d2823ea3
Noel Ohaeri <noelohaeri@gmail.com>
2023-12-18 14:03:42 -0500

    Remove commented out webpack configuration

--- next.config.js
+++ next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-// const nextConfig = {
-//     webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
-//         config.module.rules.push({
-//             test: /\.mjs$/,
-//             include: /node_modules/,
-//             type: 'javascript/auto',
-//         });
-//         return config;
-//     },
-// };
-
 module.exports = nextConfig;
