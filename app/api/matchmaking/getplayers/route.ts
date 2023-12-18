d8e9f3d5219ca4770bb22a5327c655392b5c7641
Noel Ohaeri <noelohaeri@gmail.com>
2023-12-18 14:28:20 -0500

    Add eslint-disable-next-line to POST function

--- app/api/matchmaking/getplayers/route.ts
+++ app/api/matchmaking/getplayers/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import { redisClient } from '@/app/lib/redis';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function POST(request: Request) {
     const allUsers = await redisClient.sMembers('online_users');
 
