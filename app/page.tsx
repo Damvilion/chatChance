6c03765c36d357207d2648a9720daa6d940ac7d6
Noel Ohaeri <noelohaeri@gmail.com>
2023-12-18 14:24:03 -0500

    Refactor import statement in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 'use client';
-
-import { useEffect, useState } from 'react';
-
+import React from 'react';
 export default function Home() {
     return (
         <main className='flex min-h-screen flex-col items-center justify-center p-24'>
