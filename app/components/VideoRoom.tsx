6d85e0cfcb0fb4028e3d4d467c6076f2df2086d8
Noel Ohaeri <noelohaeri@gmail.com>
2023-12-19 02:55:40 -0500

    Add useParticipants hook and participantCheck function

--- app/components/VideoRoom.tsx
+++ app/components/VideoRoom.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import '@livekit/components-styles';
 import { useTracks, VideoTrack } from '@livekit/components-react';
 import { Track } from 'livekit-client';
+import { useParticipants } from '@livekit/components-react';
 
 interface VideoRoomProps {
     matched_user: string;
@@ -17,23 +18,39 @@ const VideoRoom = ({ matched_user, stopMatching, startMatch, loading }: VideoRoo
     // const audioTrack = useTracks([Track.Source.Microphone]);
     const cameraTracks = trackRefs[0];
     const user2CameraTrack = trackRefs[1] ? trackRefs[1] : null;
+    const participants = useParticipants();
+    const participantCheck = () => {
+        if (Object.keys(participants).length >= 2) {
+            stopMatching();
+        } else {
+            return;
+        }
+    };
 
     return (
-        <div className='flex flex-col'>
-            {user2CameraTrack ? (
-                <VideoTrack style={{ width: '100%', height: '100%' }} trackRef={user2CameraTrack} />
-            ) : (
-                <video style={{ width: '100%', height: '100%', backgroundColor: 'gray' }}></video>
-            )}
+        <div className='flex flex-col w-full items-center p-3'>
+            <div className='min-h-[200px] h-[200px] w-[80%] sm:h-[215px] md:h-[350px]'>
+                {user2CameraTrack ? (
+                    <VideoTrack
+                        className='object-fit w-full h-full'
+                        // style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                        trackRef={user2CameraTrack}
+                    />
+                ) : (
+                    <video className='object-fit w-full h-full bg-slate-800'></video>
+                )}
+            </div>
 
-            {cameraTracks ? (
-                <VideoTrack style={{ width: '100%', height: '100%' }} trackRef={cameraTracks} />
-            ) : (
-                <video style={{ width: '100%', height: '100%', backgroundColor: 'gray' }}></video>
-            )}
+            <div className='min-h-[200px] h-[200px] w-[80%] sm:h-[215px] md:h-[350px]'>
+                {cameraTracks ? (
+                    <VideoTrack className='object-fit w-full h-full' trackRef={cameraTracks} />
+                ) : (
+                    <video className='object-fit w-full h-full bg-slate-800'></video>
+                )}
+            </div>
 
             <div className='flex gap-4 justify-center items-end p-5'>
-                <button className='bg-red-500 p-3 rounded-lg' onClick={stopMatching}>
+                <button className='bg-red-500 p-3 rounded-lg' onClick={participantCheck}>
                     stop matching
                 </button>
                 <button className={`${loading ? 'bg-slate-500' : 'bg-blue-400'} p-3 rounded-lg`} onClick={startMatch}>
