249093cd9415f5326adca051a2ada7e9fa26a5b0
Noel Ohaeri <noelohaeri@gmail.com>
2023-12-19 17:42:22 -0500

    Add new files and update dependencies

--- app/matchmaking/layout.tsx
+++ app/matchmaking/layout.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+'use client';
+import * as React from 'react';
+import { NextUIProvider } from '@nextui-org/react';
 
 export default function MatchMakingLayout({ children }: { children: React.ReactNode }) {
-    return <div className='h-screen flex flex-col justify-center md:justify-start'>{children}</div>;
+    return (
+        <div className='h-screen flex flex-col justify-center md:justify-start bg-slate-700'>
+            <NextUIProvider>{children}</NextUIProvider>
+        </div>
+    );
 }
