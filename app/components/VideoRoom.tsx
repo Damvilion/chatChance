d2ae83e2087ed9ec0c5f2336c0bd90f9b524240a
Noel Ohaeri <noelohaeri@gmail.com>
2023-12-20 19:40:12 -0500

    Add video and audio feed from second user

--- app/components/VideoRoom.tsx
+++ app/components/VideoRoom.tsx
@@ -1,63 +1,88 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import '@livekit/components-styles';
-import { useTracks, VideoTrack } from '@livekit/components-react';
-import { Track } from 'livekit-client';
-import { useParticipants } from '@livekit/components-react';
+import { useTracks, VideoTrack, useConnectionState, AudioTrack } from '@livekit/components-react';
+import { Track, LocalVideoTrack } from 'livekit-client';
+import { useParticipants, useLocalParticipant } from '@livekit/components-react';
 import { Button } from '@nextui-org/react';
 
 interface VideoRoomProps {
     loading: boolean;
     startMatch: () => void;
-    stopAllMatching: () => void;
-    stopAllMatchingWithLiveKit: () => void;
+    stopAllMatching: (withLiveKit: boolean) => void;
+    mediaStream: MediaStream | null;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const VideoRoom = ({ loading, startMatch, stopAllMatching, stopAllMatchingWithLiveKit }: VideoRoomProps) => {
-    const trackRefs = useTracks([Track.Source.Camera]);
-    // const audioTrack = useTracks([Track.Source.Microphone]);
-    const cameraTracks = trackRefs[0];
+const VideoRoom = ({ loading, startMatch, stopAllMatching, mediaStream }: VideoRoomProps) => {
+    const localParticipant = useLocalParticipant();
+
+    const connectionState = useConnectionState();
+
+    // Video and audio Feed from livekit room
+    const trackRefs = useTracks([Track.Source.Unknown]);
+    const audioTrack = useTracks([Track.Source.Microphone]);
+
+    // Video and audio Feed from second user
     const user2CameraTrack = trackRefs[1] ? trackRefs[1] : null;
+    const user2AudioTrack = audioTrack[1] ? audioTrack[1] : null;
+
     // Gets the participants in the LiveKit Room
     const participants = useParticipants();
+
+    // Checks if there are more than 2 participants in the room
     const participantCheck = () => {
         // If there are more than 2 participants in the room, stop all matching
         if (Object.keys(participants).length >= 2) {
-            stopAllMatchingWithLiveKit();
+            // if in LiveKit Room | Disconnect from LiveKit
+            stopAllMatching(true);
         } else {
-            stopAllMatching();
+            stopAllMatching(false);
             return;
         }
     };
 
-    return (
-        <div className='flex flex-col w-full items-center p-3'>
-            <div className='min-h-[200px] h-[200px] w-[80%] sm:h-[215px] md:h-[350px]'>
-                {user2CameraTrack ? (
-                    <VideoTrack className='object-cover w-full h-full rounded-sm' trackRef={user2CameraTrack} />
-                ) : (
-                    <video className='object-cover w-full h-full rounded-sm bg-slate-800'></video>
-                )}
-            </div>
+    useEffect(() => {
+        const connect = async () => {
+            if (connectionState === 'connected' && mediaStream) {
+                try {
+                    const localVideoTrack = new LocalVideoTrack(mediaStream!.getVideoTracks()[0]);
+                    await localParticipant.localParticipant.publishTrack(localVideoTrack);
+                } catch (e) {
+                    console.error(e);
+                }
+            }
+        };
+        connect();
+    }, [connectionState]);
 
-            <div className='min-h-[200px] h-[200px] w-[80%] sm:h-[215px] md:h-[350px]'>
-                {cameraTracks ? (
-                    <VideoTrack className='object-cover w-full h-full rounded-sm' trackRef={cameraTracks} />
-                ) : (
-                    <video className='object-cover w-full h-full bg-slate-800'></video>
-                )}
-            </div>
+    // const logTracks = () => {
+    //     console.log('Tracks: ', trackRefs);
+    //     console.log('Audio: ', audioTrack);
+    //     console.log(user2AudioTrack);
+    // };
+
+    return (
+        <div className='w-full h-full'>
+            {user2AudioTrack && <AudioTrack trackRef={user2AudioTrack} />}
+            {/* <div className='min-h-[200px] h-[200px] w-[80%] sm:h-[215px] md:h-[350px]'> */}
+            {user2CameraTrack ? (
+                <VideoTrack className='object-cover w-full h-full rounded-sm' trackRef={user2CameraTrack} />
+            ) : (
+                <video className='object-cover w-full h-full rounded-sm bg-slate-800'></video>
+            )}
 
             {/* Buttons */}
-            <div className='flex gap-4 justify-center items-end p-5'>
-                <Button color='danger' onClick={participantCheck}>
+
+            <div className='flex gap-4 justify-center items-end'>
+                <Button size='sm' color='danger' onClick={participantCheck}>
                     Stop
                 </Button>
 
-                <Button color='secondary' isLoading={loading} onClick={startMatch}>
+                <Button size='sm' color='secondary' isLoading={loading} onClick={startMatch}>
                     Start Match
                 </Button>
+                {/* <Button onClick={logTracks}>Log Tracks</Button> */}
             </div>
         </div>
     );
